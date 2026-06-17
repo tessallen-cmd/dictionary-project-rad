daacadc3aa5cba3e06073bd1a0f88bd110ea889e
tessallen-cmd <tessstablet@gmail.com>
2026-06-17 20:56:30 +0100

    set up and logo+ import bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} className="App-logo " alt="logo" />
+    </div>
+  );
+}
+
+export default App;
